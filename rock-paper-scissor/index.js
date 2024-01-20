7b5dbcd04efb19817a3c9972b9153b96c174d82c
kkekevin <kke_kevin@hotmail.com>
2024-01-19 22:57:06 -0300

    first commit

---
+++ rock-paper-scissor/index.js
@@ -0,0 +1,29 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("player");
+const pcDisplay = document.getElementById("pc");
+const resultDisplay = document.getElementById("result");
+
+
+function playRound (playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return "it's a tie!"
+    } else {
+        switch (computerSelection) {
+            case 'rock':
+                return (playerSelection === 'paper') ? "YOU WIN!" : "YOU LOSE!";
+            case 'paper':
+                return (playerSelection === 'scissors') ? "YOU WIN!" : "YOU LOSE!";
+            case 'scissors':
+                return (playerSelection === 'rock') ? "YOU WIN!" : "YOU LOSE!";
+        }
+    }
+}
+
+function game (pChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)]    
+    let result = playRound(pChoice, computerChoice);
+
+    //playerDisplay.textContent = `player: ${pChoice}`;
+    //pcDisplay.textContent = `pc: ${computerChoice}`;
+    resultDisplay.textContent = result;
+}
