3184ef20f49fbd63fca3fd9ba93451d2101c8501
kkekevin <kke_kevin@hotmail.com>
2024-02-01 21:27:54 -0300

    keep up hoover

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -5,6 +5,13 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(div);
     for (let j = 0; j < 16; j++) {
         const child = document.createElement('div');
+        child.classList.add('etch');
         div.appendChild(child);
     }
 }
+
+var etch = document.querySelectorAll('.etch');
+
+etch.forEach(cell => cell.addEventListener('mouseenter', () => {
+    cell.setAttribute('style', 'background-color: hsl(240, 12%, 50%);')
+}));
