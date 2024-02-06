a3d051f97d9b9d8484dfa17ef2da145c6b7ed236
kkekevin <kke_kevin@hotmail.com>
2024-02-06 11:46:40 -0300

    add button to create grids

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,7 +1,9 @@
 const container = document.getElementById('container');
+const buttonGrid = document.getElementById('createGrid');
 
 for (let i = 0; i < 16; i++) {
     const div = document.createElement('div');
+    div.classList.add('etch1');
     container.appendChild(div);
     for (let j = 0; j < 16; j++) {
         const child = document.createElement('div');
@@ -11,7 +13,27 @@ for (let i = 0; i < 16; i++) {
 }
 
 var etch = document.querySelectorAll('.etch');
+var parentEtch = document.querySelectorAll('.etch1');
 
 etch.forEach(cell => cell.addEventListener('mouseenter', () => {
     cell.setAttribute('style', 'background-color: hsl(240, 12%, 50%);')
 }));
+
+buttonGrid.addEventListener('click', () => {
+    let nGrid = parseInt(prompt("how many grids??"));
+    parentEtch.forEach(cell => cell.remove());
+
+    createGrid(nGrid);
+});
+
+function createGrid(nGrid) {
+    for (let i = 0; i < nGrid; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        for (let j = 0; j < nGrid; j++) {
+            const child = document.createElement('div');
+            child.classList.add('etch');
+            div.appendChild(child);
+        }
+    }
+}
