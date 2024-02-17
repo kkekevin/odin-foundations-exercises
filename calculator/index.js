56e7191585c10cf6723890487ae2aa46dce7e329
kkekevin <kke_kevin@hotmail.com>
2024-02-17 00:43:00 -0300

    get a function to obtain the result of the operation

--- calculator/index.js
+++ calculator/index.js
@@ -1,17 +1,46 @@
 const operationDisplay = document.getElementById('operation');
 const resultDisplay = document.getElementById('result');
 let n = '';
-var a, b;
+var a, b, result;
 let operator = '';
 
-function concatenate (number) {
+function concatenate(number) {
     n += number.toString();
     operationDisplay.textContent = `${n}`;
 }
 
-function op (sign) {
+function op(sign) {
     operator = sign;
     a = parseInt(n);
     n = '';
 }
 
+function calcul() {
+    b = parseInt(n);
+    n = '';
+    operationDisplay.textContent = `${n}`;
+    if (a != null) {
+        switch (operator) {
+            case '+':
+                result = a + b;
+                resultDisplay.textContent = `${result}`;
+                break;
+            case '-':
+                result = a - b;
+                resultDisplay.textContent = `${result}`;
+                break;
+            case 'x':
+                result = a * b;
+                resultDisplay.textContent = `${result}`;
+                break;
+            case '/':
+                result = a / b;
+                resultDisplay.textContent = `${result}`;
+                break;
+            default:
+                break;
+        }
+    } else {
+        resultDisplay.textContent = `${b}`;
+    }
+}
