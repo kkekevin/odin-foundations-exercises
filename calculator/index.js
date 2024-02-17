0497898dbb76ec8ef156e28e6035dbac49afe7e2
kkekevin <kke_kevin@hotmail.com>
2024-02-17 00:13:43 -0300

    select one of the four operators

--- calculator/index.js
+++ calculator/index.js
@@ -1,8 +1,17 @@
 const operationDisplay = document.getElementById('operation');
 const resultDisplay = document.getElementById('result');
 let n = '';
+var a, b;
+let operator = '';
 
 function concatenate (number) {
     n += number.toString();
     operationDisplay.textContent = `${n}`;
 }
+
+function op (sign) {
+    operator = sign;
+    a = parseInt(n);
+    n = '';
+}
+
